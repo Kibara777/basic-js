4291d9ee6c47a6f5772ff5edc811d1017a07dc16
Kibara777 <olgaram07@mail.ru>
2022-10-11 00:54:23 +0300

    add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+    
+  }
+  let newArr = Array.from(arr);
+  let sequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  for (let i = 0; i < newArr.length; i++) {
+    if (sequences.includes(newArr[i])) {
+      newArr = updateArray(newArr, i, newArr[i]);
+    }
+  }
+  return newArr.filter(el => el);
+
+  function updateArray(arr, i, option) {
+    delete arr[i]
+    if (i >= 0 && i < arr.length) {
+      if (option == sequences[0]) {
+        delete arr[i + 1];
+      }
+      if (option == sequences[1]) {
+        delete arr[i - 1];
+      }
+      if (option == sequences[2]) {
+        arr[i] = arr[i + 1];
+      }
+      if (option == sequences[3]) {
+        arr[i] = arr[i - 1];
+      }
+    }
+    return arr
+  }
 }
 
 module.exports = {
