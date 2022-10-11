adf38488242ae765c9079464dde4339d969cfdc6
Kibara777 <olgaram07@mail.ru>
2022-10-11 15:07:37 +0300

    add solutions for three tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,117 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(line,key) {
+
+    if(typeof(line) != 'string' && typeof(key) != 'string' || !line || !key) throw new Error('Incorrect arguments!')
+    if(line.length == 0 || key.length == 0) throw new Error('Incorrect arguments!')
+
+    let str3 = ''
+    let keyarray = []
+   let array = alphabet()
+
+
+	  line = line.toUpperCase()
+	  key = key.toUpperCase()
+	  keyarray = key.split('')
+	  key  = keychange(line,keyarray)
+
+	  for(let c = 0; c < line.length ; c++) {
+      
+      if(line[c].charCodeAt() < 65 || line[c].charCodeAt() > 90) {
+        str3 += line[c]
+        continue
+      }
+      str3 +=	array[key[c].charCodeAt() - 65][line[c].charCodeAt() - 65]
+      
+    }
+    
+  
+  console.log(str3)
+  
+  if(this.direction == false) {
+    return str3.split('').reverse().join('')
+  }
+ 
+  return str3
+  
+  }
+  decrypt(answer,key) {
+    
+    if(typeof(answer) != 'string' && typeof(key) != 'string' || !answer || !key) throw new Error('Incorrect arguments!')
+    if(answer.length == 0 || key.length == 0) throw new Error('Incorrect arguments!')
+    let reverseRes = ''
+    let keyarray = []
+    let array = alphabet()
+
+
+	  answer = answer.toUpperCase()
+	  key = key.toUpperCase()
+	  keyarray = key.split('')
+
+	  key = keychange(answer,keyarray)
+
+	  for(let q = 0; q < answer.length ; q++) {
+      
+      if(answer[q].charCodeAt() < 65 || answer[q].charCodeAt() > 90) {
+        reverseRes += answer[q]
+        continue
+      }
+      reverseRes += array[0][array[key[q].charCodeAt() - 65].indexOf(answer[q])]
+      
+    }
+    
+
+    
+    if(this.direction == false) {
+      return reverseRes.split('').reverse().join('')
+    }
+
+
+  return reverseRes
+  
   }
 }
+
+
+function alphabet() {
+	let array = []
+	let str ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let str2 = ''
+  let	keyarray = []
+	for (i = 0 ; i < 26;i++) {
+		str2 = (str.slice(i) + str.slice(0,i))
+		array.push(str2)
+		str2 = ''
+	}
+	return array 
+}
+
+function keychange(answer, keyarray) {
+	let key = ''
+	let a = 0
+	for(let b = a= 0; b < answer.length; b++) {
+
+		if(+answer[b].charCodeAt() > 64 && +answer[b].charCodeAt() < 91) {
+			key += keyarray[a];
+			a++;
+		}else {
+			key += ' ';
+		}
+		if(a == keyarray.length ) {
+			a = 0;
+		}
+		
+	}
+	return key
+}
+
+new VigenereCipheringMachine(false)
+
 
 module.exports = {
   VigenereCipheringMachine
